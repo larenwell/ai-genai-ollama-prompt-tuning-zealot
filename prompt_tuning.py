d14c6c3c449740540a0efb09022c214e82622e50
Jose Quispe <jalbertoqz@gmail.com>
2025-07-18 15:51:35 -0500

    [ Added raw prompts version ]

--- prompt_tuning.py
+++ prompt_tuning.py
@@ -2,4 +2,4 @@ from utils.common import run_prompt_tuning_evaluation
 
 if __name__ == "__main__":
     sample_size = 3
-    run_prompt_tuning_evaluation(sample_size)
+    run_prompt_tuning_evaluation(sample_size, version=1)
